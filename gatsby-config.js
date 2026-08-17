3b963c234258526837211c2403dc6300dbdc8514
igor <stari4ok.stari4ok@gmail.con>
2026-08-17 19:58:13 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mrii u Dii`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "\u00161O-Y3TFyyJHo8XDhVdEjzkDwsicVvdNfiz07nBVs-WU",
+      "spaceId": "h7tvj4uy0ui4"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
+};
