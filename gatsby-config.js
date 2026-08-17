041d51c15e0e37307fa6f8f0c169a0903cebda3d
igor <stari4ok.stari4ok@gmail.con>
2026-08-17 20:30:18 +0300

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "\u00161O-Y3TFyyJHo8XDhVdEjzkDwsicVvdNfiz07nBVs-WU",
+      "accessToken": "1O-Y3TFyyJHo8XDhVdEjzkDwsicVvdNfiz07nBVs-WU",
       "spaceId": "h7tvj4uy0ui4"
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
